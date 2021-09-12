f6479489e6f4e04fa986c58f43bbf114a4e53472
ionut1912 <Facultate2018>
2021-09-12 20:06:55 +0300

    second comit

---
+++ src/components/Order/OrderForm.js
@@ -0,0 +1,16 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import Form  from   "../../layouts/Form"
+export default function OrderForm(){
+    return(
+<Form>
+<Grid container>
+<Grid item xs={6}>
+    <Grid item xs={6}>
+        
+    </Grid>
+</Grid>
+</Grid>
+</Form>
+);
+}
