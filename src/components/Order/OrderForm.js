69bc3643e470c079ab548b30da8268811ad7c7a9
ionut1912 <nitescucornelionut19@gmail.com>
2021-09-16 18:01:40 +0300

    Input displayed

--- src/components/Order/OrderForm.js
+++ src/components/Order/OrderForm.js
@@ -1,14 +1,21 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 import Form  from   "../../layouts/Form"
+import Input from "../../controls/Input"
 export default function OrderForm(){
     return(
 <Form>
 <Grid container>
 <Grid item xs={6}>
+<Input disabled label="OrderNumber" name="orderNumber">
+
+</Input>
+</Grid>
     <Grid item xs={6}>
-        
-    </Grid>
+        <Input disabled label="Grand Total" name="gTotal">
+
+        </Input>
+    
 </Grid>
 </Grid>
 </Form>
